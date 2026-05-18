43abc9ddbcdcef2411d75ce4d7cabff7c3b57240
LiuTianshi123 <2276935812@qq.com>
2026-05-18 14:11:01 +0800

    Upload supervise-system/src/main/java/com/zgwx/supervise/config/AutowiringSpringBeanJobFactory.java

---
+++ supervise-system/src/main/java/com/zgwx/supervise/config/AutowiringSpringBeanJobFactory.java
@@ -0,0 +1,30 @@
+package com.zgwx.supervise.config;
+
+import org.quartz.spi.TriggerFiredBundle;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.scheduling.quartz.SpringBeanJobFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 让 Quartz Job 支持 Spring 依赖注入
+ */
+@Component
+public class AutowiringSpringBeanJobFactory extends SpringBeanJobFactory
+        implements ApplicationContextAware {
+
+    private AutowireCapableBeanFactory beanFactory;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.beanFactory = applicationContext.getAutowireCapableBeanFactory();
+    }
+
+    @Override
+    protected Object createJobInstance(TriggerFiredBundle bundle) throws Exception {
+        Object job = super.createJobInstance(bundle);
+        beanFactory.autowireBean(job);
+        return job;
+    }
+}
