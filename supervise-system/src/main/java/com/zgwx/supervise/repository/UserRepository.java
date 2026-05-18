f645aed890996253130e568fa2991aa1823a1367
LiuTianshi123 <2276935812@qq.com>
2026-05-18 14:11:25 +0800

    Upload supervise-system/src/main/java/com/zgwx/supervise/repository/UserRepository.java

---
+++ supervise-system/src/main/java/com/zgwx/supervise/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.zgwx.supervise.repository;
+
+import com.zgwx.supervise.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
